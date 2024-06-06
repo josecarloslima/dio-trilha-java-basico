494fde8e300260b25ad8b30de69ef409605ca02d
Jose Carlos Linma <josecarlosrlima@gmail.com>
2024-06-06 19:01:33 -0300

    Adição de aula sobre operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Concatenização
+        //String nomeCompleto = "Linguagem" + "Java";
+        //System.out.println(nomeCompleto);
+
+        //Concatenação e soma
+        //String concatenacao = "?";
+        //System.out.println(concatenacao);
+
+	    //concatenacao = 1+1+1+"1";
+        //System.out.println(concatenacao);
+
+	    //concatenacao = 1+"1"+1+1;
+        //System.out.println(concatenacao);
+
+	    //concatenacao = 1+"1"+1+"1";
+        //System.out.println(concatenacao);
+
+	    //concatenacao = "1"+1+1+1;
+        //System.out.println(concatenacao);
+
+	    //concatenacao = "1"+(1+1+1);
+        //System.out.println(concatenacao);
+
+        //Operadores Unários
+        int numero = 5;
+        //System.out.println(numero);
+        //System.out.println(- numero);
+        numero = - numero;
+        System.out.println(numero);
+        //numero = + numero;
+        numero = numero * -1;
+        System.out.println(numero);
+    }
+}
