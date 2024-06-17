b234f6855d52fcff6369ac69f1f55ff536f48550
Jose Carlos Linma <josecarlosrlima@gmail.com>
2024-06-17 19:13:28 -0300

    adição do javadoc com exercícios

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1> Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022 
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm primeiro parâmetro do método
+     * @param numeroDois segundo parâmetro do método
+     * @return o resultado deste método é a soma dos dois valores (int)
+     */
+
+     public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+     }    
+}
---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+         /**
+          * documentação usa os dois asteriscos no inicio
+          * e ela fica dentro da classe ou no método
+          */
+
+    public static void main(String[] args) throws Exception {
+        // cometário em uma única linha (one line)
+
+        /*
+         * comentário mais detalhado que usa mútliplas linhas
+         * para explicar o que for
+         */
+
+    }
+}
