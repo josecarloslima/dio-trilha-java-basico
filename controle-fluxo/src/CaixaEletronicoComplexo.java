83856632087b6fada31890092adec29522bad717
Jose Carlos Linma <josecarlosrlima@gmail.com>
2024-06-25 19:20:48 -0300

    adição de exercícios de estrutura condicional composta

---
+++ controle-fluxo/src/CaixaEletronicoComplexo.java
@@ -0,0 +1,13 @@
+public class CaixaEletronicoComplexo {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo: R$ " + saldo);
+        }else
+        System.out.println("Saldo Insuficiente");
+    }
+    
+}
