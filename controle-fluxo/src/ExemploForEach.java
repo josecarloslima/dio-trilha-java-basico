e989777f16d9cd3c038e918ba16338a09b4cffe5
josecarloslima <josecarloslima@gmail.com>
2024-07-03 18:21:14 -0300

    adição dos exemplos for

---
+++ controle-fluxo/src/ExemploForEach.java
@@ -0,0 +1,9 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos [] = { "Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (String aluno : alunos) {
+            System.out.println("O nome do aluno é " + aluno);
+        }
+    }
+}
